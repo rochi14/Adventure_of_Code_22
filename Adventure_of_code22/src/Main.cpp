d82834e9a23822ab230e3bdfe29c68029dc66a04
rochi14 <33075172+rochi14@users.noreply.github.com>
2022-12-03 16:16:32 -0800

    Day3

--- Adventure_of_code22/src/Main.cpp
+++ Adventure_of_code22/src/Main.cpp
@@ -121,9 +121,120 @@ public:
         std::cout << "Result :: "<< result << "\n";
     }
 };
+class Day3 {
+public:
+    /*int calcValueForString(string sub1, string sub2) {
+        //string sub1 = s1.substr(0, s1.length() / 2);
+        //string sub2 = s1.substr(s1.length() / 2, s1.length());
+        int a1[52] = { 0 }; // first character is blank ;first 26 are lower case and rest upper case 
+        int a2[52] = { 0 };
+        int i =0, j =0;
+        for (i = 0; i < (int)sub1.length(); i++) {
+            int ch = (int)sub1[i];
+            if ( ch>= 65 && ch <= 90) {
+                int diff = ch - 65;
+                a1[25 + diff + 1]++;
+            }
+            if (ch >= 97 && ch <= 122) {
+                int diff = ch - 97;
+                a1[diff]++;
+            }
+        }
+        for (i = 0; i < (int)sub2.length(); i++) {
+            int ch = (int)sub2[i];
+            if (ch >= 65 && ch <= 90) {
+                int diff = ch - 65;
+                a2[25 + diff + 1]++;
+            }
+            if (ch >= 97 && ch <= 122) {
+                int diff = ch - 97;
+                a2[diff]++;
+            }
+        }
+        for (i = 0; i < 52; i++) {
+            if (a1[i] != 0 && a2[i] != 0) {
+                return i+1; 
+            }
+        }
+        return 0;
+    }*/
+    void checkFreqOfCharacters(string sub1, int (&array)[52]) {
+        int i = 0, j = 0;
+        for (i = 0; i < (int)sub1.length(); i++) {
+            int ch = (int)sub1[i];
+            if (ch >= 65 && ch <= 90) {
+                int diff = ch - 65;
+                array[25 + diff + 1]++;
+            }
+            if (ch >= 97 && ch <= 122) {
+                int diff = ch - 97;
+                array[diff]++;
+            }
+        }
+    }
+    int calcValueForString(string sub1, string sub2) {
+        //string sub1 = s1.substr(0, s1.length() / 2);
+        //string sub2 = s1.substr(s1.length() / 2, s1.length());
+        int a1[52] = { 0 }; // first character is blank ;first 26 are lower case and rest upper case 
+        int a2[52] = { 0 };
+        checkFreqOfCharacters(sub1, a1);
+        checkFreqOfCharacters(sub2, a2);
+        for (int i = 0; i < 52; i++) {
+            if (a1[i] != 0 && a2[i] != 0) {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+    void task() {
+        fstream newfile;
+        newfile.open("D:/Personal/Adventure_of_Code_22/Adventure_of_code22/input_text_day3.txt", ios::in);  // open a file to perform write operation using file object
+        int result = 0;
+        if (newfile.is_open()) {   //checking whether the file is open
+            string tp;
+            while (getline(newfile, tp)) {
+                int val = calcValueForString(tp.substr(0,tp.length()/2),tp.substr(tp.length()/2+1));
+                result += val;
+            }
+        }
+        std::cout << "Result :: "<< result << "\n";
+    }
+    int calcValueForString3(string s1, string s2, string s3) {
+        int a1[52] = { 0 }; // first character is blank ;first 26 are lower case and rest upper case 
+        int a2[52] = { 0 };
+        int a3[52] = { 0 };
+        checkFreqOfCharacters(s1, a1);
+        checkFreqOfCharacters(s2, a2);
+        checkFreqOfCharacters(s3, a3);
+        for (int i = 0; i < 52; i++) {
+            if (a1[i] != 0 && a2[i] != 0 && a3[i] != 0) {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+    void task2() {
+        fstream newfile;
+        newfile.open("D:/Personal/Adventure_of_Code_22/Adventure_of_code22/input_text_day3.txt", ios::in);  // open a file to perform write operation using file object
+        int result = 0;
+        if (newfile.is_open()) {   //checking whether the file is open
+            string tp;
+            while (getline(newfile, tp)) {
+                string s1 = tp;
+                getline(newfile, tp);
+                string s2 = tp;
+                getline(newfile, tp);
+                string s3 = tp;
+                int val = calcValueForString3(s1,s2,s3);
+                result += val;
+            }
+        }
+        std::cout << "Result :: " << result << "\n";
+    }
+};
 int main() {
-    Day2 obj;
-    obj.task();
+    Day3 obj;
+    obj.task2();
     std::cin.get();
 	return 0;
 }
